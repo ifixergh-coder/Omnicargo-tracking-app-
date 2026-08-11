ae43ba5438456644fae8607f331ed2c7db505dbf
ifixergh-coder <ifixergh@gmail.com>
2026-08-11 17:52:40 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import TrackPage from './pages/TrackPage'
 import DriverPage from './pages/DriverPage'
+import StaffLogin from './pages/StaffLogin'
+import NewShipment from './pages/NewShipment'
+import ProtectedRoute from './components/ProtectedRoute'
 
 export default function App() {
   return (
@@ -11,6 +14,8 @@ export default function App() {
         <Route path="/track" element={<TrackPage />} />
         <Route path="/track/:trackingNumber" element={<TrackPage />} />
         <Route path="/driver" element={<DriverPage />} />
+        <Route path="/staff/login" element={<StaffLogin />} />
+        <Route path="/staff/shipments/new" element={<ProtectedRoute><NewShipment /></ProtectedRoute>} />
       </Routes>
     </BrowserRouter>
   )
