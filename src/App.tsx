394a4654de065713cacf184b85064e987826159c
ifixergh-coder <ifixergh@gmail.com>
2026-08-11 19:02:29 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import TrackPage from './pages/TrackPage'
 import DriverPage from './pages/DriverPage'
 import StaffLogin from './pages/StaffLogin'
+import StaffShipments from './pages/StaffShipments'
 import NewShipment from './pages/NewShipment'
 import ShipmentLabel from './pages/ShipmentLabel'
 import ShipmentInvoice from './pages/ShipmentInvoice'
@@ -17,6 +18,7 @@ export default function App() {
         <Route path="/track/:trackingNumber" element={<TrackPage />} />
         <Route path="/driver" element={<DriverPage />} />
         <Route path="/staff/login" element={<StaffLogin />} />
+        <Route path="/staff/shipments" element={<ProtectedRoute><StaffShipments /></ProtectedRoute>} />
         <Route path="/staff/shipments/new" element={<ProtectedRoute><NewShipment /></ProtectedRoute>} />
         <Route path="/staff/shipments/:id/label" element={<ProtectedRoute><ShipmentLabel /></ProtectedRoute>} />
         <Route path="/staff/shipments/:id/invoice" element={<ProtectedRoute><ShipmentInvoice /></ProtectedRoute>} />
